777e75fa224369e79d6b4a89c330e4e8428fa2e2
Gabri Botha <gabri@principalsoftware.co.za>
2014-10-30 22:36:58 +0200

    index.js:  - prefetch page 2.  - right page zone now dependant on window

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
 $(function() {
-    var body = $("body");
-    var right_page_zone = body.width() - 40;
+    var target = $("div.container");
+    var right_page_zone = $(window).width() - 40;
     var page_right_div = $(".page_right");
     //var 
     
-    $("body").mousemove(function(e){
-        if (e.pageX > right_page_zone) {
-            page_right_div.css('display','block');
-        } else {
-            page_right_div.css('display','none');
+    $.ajax({
+        url: 'page1_2.html',
+        type: 'GET',
+        dataType: 'html',
+        success: function(page1_2_html) {
+            $(window).mousemove(function(e){
+                if (e.pageX > right_page_zone) {
+                    page_right_div.css('display','block');
+                } else {
+                    page_right_div.css('display','none');
+                }
+            });
+
+            page_right_div.click(function(e){
+                //window.location = "page1_2.html";
+                //$('body').html(page1_2_html);
+            });
         }
     });
     
-    page_right_div.click(function(e){
-        window.location = "page1_2.html";
-    });
     
     //$.stratus({
     //    links: 'http://soundcloud.com/foofighters/sets/wasting-light'
